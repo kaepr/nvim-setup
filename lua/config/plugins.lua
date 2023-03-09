fb49f82ecac8104ecb469f781e51840195571e75
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-03-09 13:41:36 +0530

    add lsp zero

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -77,8 +77,27 @@ plugins = {
             })
             require("telescope").load_extension("fzf")
         end
-    } 
+    },
 
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        },
+        
+        config = function()
+            require("ide.lsp")
+        end
+    }
 }
 
 require("lazy").setup(plugins)
