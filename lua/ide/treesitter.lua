45a5d0115cad69fe3551228899637a036562c17f
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-03-14 01:46:42 +0530

    fix ejs

--- lua/ide/treesitter.lua
+++ lua/ide/treesitter.lua
@@ -1,19 +1,32 @@
 require('nvim-treesitter.configs').setup({
-  -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
+    -- A list of parser names, or "all"
+    ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust" },
+    incremental_selection = { enable = true },
+    textobjects = { enable = true },
+    indent = { enable = false },
+    sync_install = false,
+    -- Have treesitter CLI installed
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    additional_vim_regex_highlighting = true
+})
 
-  incremental_selection = {enable = true},
-  textobjects = {enable = true},
-  indent = {enable = false},
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-  sync_install = false,
+parser_config.embedded_template = {
+    install_info = {
+        url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+        files = { "src/parser.c" },
+        requires_generate_from_grammar = false,
+    },
+}
 
-  -- Have treesitter CLI installed
-  auto_install = true,
+vim.treesitter.language.register("embedded_template", "ejs")
 
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  additional_vim_regex_highlighting = true
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = "*.ejs",
+    command = "set filetype=ejs"
 })
